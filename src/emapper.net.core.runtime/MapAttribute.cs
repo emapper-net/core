42ffed564a7fe815d4507f2ea3c60535803a155d
Pedro Gomes <pedro.gomes@talkdesk.com>
2022-12-26 08:24:17 +0000

    feat: create runtime to support mapping

--- src/emapper.net.core.runtime/MapAttribute.cs
+++ src/emapper.net.core.runtime/MapAttribute.cs
@@ -4,6 +4,9 @@ namespace emapper.net;
 [AttributeUsage(AttributeTargets.Interface, AllowMultiple = true)]
 public class MapAttribute : Attribute
 {
+    public string? Target { get; set; } = null;
+
     public Type? Using { get; set; } = null;
 
+    public string? WithExpression { get; set; } = null;
 }
